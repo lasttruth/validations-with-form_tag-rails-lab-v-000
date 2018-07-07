d3ad3b9a4f6ff48fa8b5916675e3d3c54ad568b9
Robin Doirin <robindoirin@gmail.com>
2018-07-07 21:03:26 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,3 @@
 class Post < ActiveRecord::Base
-  validates :name, presence: true
-  validates :content, length: { minimum: 100 }
-  validates :category, inclusion: { :in => ['Fiction', 'Non-Fiction']}
+  validates :title, presence: true
 end
