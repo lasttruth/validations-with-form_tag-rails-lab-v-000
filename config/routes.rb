8238997742cc12d2cff69454ee37f25ea0926b4e
Robin Doirin <robindoirin@gmail.com>
2018-07-07 21:28:14 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get "/author/:id", to: "author#show", as: "author"
+  resources :authors, only: [:new, :show, :edit, :create, :update]
+  resources :posts, only: [:new, :show, :edit, :create, :update]
 end
